e6d42496fc85cb72e3d15adac136825fddf7d311
meowdule <meowdule73@gmail.com>
2026-04-30 16:41:42 +0900

    SPA louter 404 error fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import * as AppData from "./lib/appData";
 import "./styles/global.css";
 
-const raw = import.meta.env.BASE_URL;
-const basename = raw === "/" ? undefined : raw.replace(/\/$/, "") || undefined;
-
 void AppData.init().then(() => {
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
-      <BrowserRouter basename={basename}>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </StrictMode>,
   );
 });
